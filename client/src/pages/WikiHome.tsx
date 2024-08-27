bb5f5eb1cfea0293b194019806c993a752984b08
ChristofferBillman <christoffer.billman@gmail.com>
2024-08-27 21:18:21 +0200

    feat: created component WikiHome.

---
+++ client/src/pages/WikiHome.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import PageCard from '../components/PageCard'
+import { Row } from '../components/common/Layout'
+import Page from '../types/Page'
+import PageAPI from '../network/PageAPI'
+
+export default function WikiHome() {
+
+	const [pages, setPages] = useState<Page[]>([])
+	const [error, setError] = useState('')
+
+	useEffect(() => {
+		PageAPI.all(pages => setPages(pages), err => setError(err))
+	},[])
+
+	if(error != '') return (
+		<>
+			<h1>An error occured:</h1>
+			<h5>{error}</h5>
+		</>
+	)
+
+	return (
+		<>
+			<h1 style={{marginBottom: '1rem'}}>All Pages</h1>
+			<Row style={{ flexWrap: 'wrap', gap: '2rem', padding: 0 }}>
+				{pages.map(page => <PageCard key={page._id} page={page}/>)}
+			</Row>
+		</>
+	)
+}
