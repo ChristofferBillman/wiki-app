0fd12ecc381ab866178a0ecf3774e641617e3210
ChristofferBillman <christoffer.billman@gmail.com>
2024-08-27 21:20:48 +0200

    feat: created homepage that the user goes to when logging in.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,31 +1,66 @@
 import { useEffect, useState } from 'react'
-import PageCard from '../components/PageCard'
-import { Row } from '../components/common/Layout'
-import Page from '../types/Page'
-import PageAPI from '../network/PageAPI'
+import { Column, Filler, Row } from '../components/common/Layout'
+import SearchBar from '../components/SearchBar'
+
+import useUser from '../contexts/UserContext'
+
+import Placeholder from '../assets/img/placeholder.jpg'
+import { useNavigate } from 'react-router-dom'
+import Button from '../components/common/Button'
+import { Cogwheel, Person, Plus } from '../assets/Icons'
+import WikiAPI from '../network/WikiAPI'
+import Wiki from '../types/wiki'
+import WikiCard from '../components/WikiCard'
 
 export default function Home() {
 
-	const [pages, setPages] = useState<Page[]>([])
-	const [error, setError] = useState('')
+	const [wikis, setWikis] = useState<Wiki[]>([])
+	const [error, setError] = useState<string>('')
 
 	useEffect(() => {
-		PageAPI.all(pages => setPages(pages), err => setError(err))
-	},[])
+		WikiAPI.all(wikis => setWikis(wikis), err => setError(err))
+	}, [])
 
-	if(error != '') return (
-		<>
-			<h1>An error occured:</h1>
-			<h5>{error}</h5>
-		</>
-	)
+	const navigate = useNavigate()
+
+	const userContext = useUser()
+	const username = userContext.user.name
 
 	return (
 		<>
-			<h1 style={{marginBottom: '1rem'}}>All Pages</h1>
-			<Row style={{ flexWrap: 'wrap', gap: '2rem', padding: 0 }}>
-				{pages.map(page => <PageCard key={page._id} page={page}/>)}
-			</Row>
+
+			<Column>
+				<Row>
+					<SearchBar/>
+					<Filler/>
+					<Button
+						text='Create Wiki'
+						color='var(--primary)'
+						icon={<Plus/>}
+						onClick={() => navigate('/wiki/create')}
+					/>
+					<Button
+						text={username}
+						outline
+						icon={<Person color='var(--black)'/>}
+					/>
+					<Button
+						outline
+						icon={<Cogwheel color='var(--black)'/>}
+						onClick={() => navigate('/settings')}
+					/>
+				</Row>
+				<h1 style={{color: 'var(--white)'}}>Welcome {username}</h1>
+				<img
+					src={Placeholder}
+					style={{position: 'absolute', width: '100vw', height: '400px', objectFit: 'cover', top: 0, left: 0, zIndex: -1, filter: 'brightness(0.6)'}}
+				/>
+
+				<div style={{height: '200px'}}/>
+
+				<h1>Your Wikis</h1>
+				<>{wikis.map(wiki => <WikiCard wiki={wiki} key={wiki._id}/>)}</>
+			</Column>
 		</>
 	)
 }
