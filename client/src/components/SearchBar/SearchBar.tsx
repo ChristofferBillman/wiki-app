8c3702b75f6d1ceb4a5f1d3de0974d67dbfcf056
ChristofferBillman <christoffer.billman@gmail.com>
2024-08-29 15:01:52 +0200

    feat: added option to search in a wiki or in all wikis.

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -6,17 +6,20 @@ import useToast from '../../contexts/ToastContext'
 import Page from '../../types/Page'
 
 import style from './SearchBar.module.css'
-import {Row} from '../common/Layout'
+import {Filler, Row} from '../common/Layout'
 import useOutsideClick from '../../hooks/useOutsideClick'
 import {SearchResult} from './SearchResult'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import wikiAPI from '../../network/WikiAPI'
 import TransitionLifecycle from '../common/TransitionLifecycle'
+import PageAPI from '../../network/PageAPI'
 
 export function SearchBar() {
 
 	const [searchQuery, setSearchQuery] = useState('')
 	const [searchResults, setSearchResults] = useState<Page[]>([])
+
+	const { wikiname } = useParams()
 
 	const [searchIsFocused, setSearchIsFocused] = useState(false)
 
@@ -26,19 +29,42 @@ export function SearchBar() {
 	const navigate = useNavigate()
 
 	const ref = useRef(null)
-	useOutsideClick(ref, () => {
-		setSearchIsFocused(false)
-	})
+	useOutsideClick(ref, () => setSearchIsFocused(false))
 
-	const search = (str: string) => {
+	// If we are currently browsing in a wiki, search only in it by default, otherwise all wikis.
+	const [searchMode, setSearchMode] = useState<'wiki' | 'everything'>(wikiname ? 'wiki' : 'everything')
+
+	const toggleSearchMode = () => {
+		if(searchMode == 'wiki') {
+			setSearchMode('everything')
+			search(searchQuery, 'everything')
+		}
+		else {
+			setSearchMode('wiki')
+			search(searchQuery, 'wiki')
+		}
+	}
+
+	const search = (str: string, searchMode: 'wiki' | 'everything') => {
 		const query = str.trim()
-		if (query.length > 0) {
-			SearchAPI.search(query, pages => {
-				setSearchResults(pages)
-			}
-			, err => toast(err, 'error'))
-		} else {
+
+		if (query.length == 0) {
 			setSearchResults([])
+			return
+		}
+
+		if(searchMode == 'everything') {
+			SearchAPI.search(query,
+				pages => setSearchResults(pages),
+				err => toast(err, 'error'))
+		}
+		else {
+			if(!wikiname) throw new Error('Wikiname was not found in params.')
+
+			PageAPI.allFromWiki(wikiname,
+				pages => setSearchResults(pages),
+				err => toast(err, 'error')
+			)
 		}
 	}
 
@@ -46,24 +72,37 @@ export function SearchBar() {
 		<>
 			<Row id={style.searchContainer} forwardRef={ref}>
 				<Input
-					onFocus={() => setSearchIsFocused(true)}
-					placeholder='Search Everything'
+					onFocus={() => {
+						const mode = wikiname ? 'wiki' : 'everything'
+						setSearchMode(mode)
+						search(searchQuery, mode)
+						setSearchIsFocused(true)
+					}}
+					placeholder={wikiname ? 'Search in ' + wikiname : 'Search Everything'}
 					style={{width: '500px', zIndex: 100}}
 					value={searchQuery}
 					name='Search'
 					setValue={e => {
 						setSearchQuery(e.target.value)
-						search(e.target.value)
+						search(e.target.value, searchMode)
 					}}
 				/>
 
 				<Transition visible={visible} id={style.searchResults}>
-					<p
-						className={style.descriptionText}
-						style={{margin: '1rem 0 0 1rem'}}
-					>
-						{searchResults.length} Results
-					</p>
+					<Row style={{padding: '1rem 1rem 0 1rem'}}>
+						<p className={style.descriptionText}>
+							{searchResults.length} Results {searchMode == 'wiki' ? '(in ' + wikiname + ')' : '(all wikis)'}
+						</p>
+
+						<Filler/>
+
+						<h4
+							className={style.clickableText}
+							onClick={toggleSearchMode}
+						>
+							{wikiname ? (searchMode == 'wiki' ? 'Search everything' : 'Search in this wiki') : ''}
+						</h4>
+					</Row>
 					{searchResults.map(page => (
 						<div
 							onClick={() => {
