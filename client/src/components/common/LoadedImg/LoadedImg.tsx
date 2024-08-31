ef93cf6aff36a75f3c1e6ccdcc3521d4b06175fc
ChristofferBillman <christoffer.billman@gmail.com>
2024-08-31 21:10:37 +0200

    feat: added error state to LoadedImg

--- client/src/components/common/LoadedImg/LoadedImg.tsx
+++ client/src/components/common/LoadedImg/LoadedImg.tsx
@@ -10,6 +10,11 @@ interface Props {
 export function LoadedImg({ src, imgStyle, style, alt }: Props): ReactNode {
 
 	const [loading, setLoading] = useState(true)
+	const [errored, setErrored] = useState(false)
+
+	if(errored) return (
+		<div className='errored' style={{display: loading ? 'block' : 'none', height: '100%', width: '100%'}}/>
+	)
 
 	return (
 		<div style={{height: '100%', width: '100%', ...style}}>
@@ -18,6 +23,7 @@ export function LoadedImg({ src, imgStyle, style, alt }: Props): ReactNode {
 				alt={alt}
 				style={{...{display: loading ? 'none' : 'block', width: '100%'}, ...imgStyle}}
 				onLoad={() => setLoading(false)}
+				onError={() => setErrored(true)}
 			/>
 			<div className='loader' style={{display: loading ? 'block' : 'none', height: '100%', width: '100%'}}/>
 		</div>
